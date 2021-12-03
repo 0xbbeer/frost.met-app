768c21f8cbcf7983cfebad36b2045cb8ef6cb05a
Anton <dev-vinokurov@yandex.ru>
2021-12-03 16:08:24 +0300

    DEV: added ability for geting data from api to db

--- app/frost_met_app/models.py
+++ app/frost_met_app/models.py
@@ -1,3 +1,15 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Stations(models.Model):
+    station_id = models.CharField(default="", max_length=512, blank=True)
+    country = models.CharField(default="", max_length=512, blank=True)
+    geometry = models.CharField(default="", max_length=512, blank=True)
+    municipality = models.CharField(default="", max_length=512, blank=True)
+    stationholder = models.CharField(default="-", max_length=512, blank=True)
+    validfrom = models.DateTimeField(default="", blank=True)
+
+    def __str__(self):
+
+        return self.station_id
+
--- app/frost_met_app/views.py
+++ app/frost_met_app/views.py
@@ -3,12 +3,17 @@
 from django.shortcuts import render
 # from django.views.generic.base import View
 from django.views.generic import TemplateView
-
-# Create your views here.
+from frost_met_app.models import Stations
 
 
 class MainPage(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = dict()
+        context["stations"] = Stations.objects.all().order_by(
+            'validfrom')[:10]
+        return context
 
 
 class NotFound(TemplateView):
